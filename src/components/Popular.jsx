a2eb4eac55d08528bd3a820f2ba30fb0564281cb
biggiech33se246 <kevinj14902@gmail.com>
2024-07-20 14:01:57 -0300

    added react splidecarousels and profile cards for popular recipes

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+import styled from "styled-components";
+import {Splide, SplideSlide} from '@splidejs/react-splide';
+import "@splidejs/splide/dist/css/splide.min.css";
 
 function Popular() {
 
@@ -12,19 +15,68 @@ function Popular() {
       const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=5`);
       const data = await api.json();
       setPopular(data.recipes);
+      console.log(data.recipes);
     };
 
     return (
       <div>
-        {popular.map((recipe) => {
-          return(
-            <div>
-              <p>{recipe.title}</p>
-            </div>
-          );
-        })}
-      </div>
+          <Wrapper>
+            <h3>Popular Recipes</h3>
+
+            <Splide options={{perPage: 2}}>
+
+            {popular.map((recipe) => {
+              return(
+                <SplideSlide>
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt="recipe.title" />
+                </Card>
+                </SplideSlide>
+              );
+            })}
+            </Splide>
+          </Wrapper>
+    </div>
     );
   }
+
+  const Wrapper = styled.div`
+    margin 4rem 0rem;
+  `;
+
+  const Card = styled.div`
+    min-height: 25rem;
+    border-radius: 2rem;
+    overflow: hidden;
+    position: relative;
+
+    img {
+      border-radius: 2rem;
+      position: absolute;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+    
+    p{
+      position: absolute;
+      z-index: 10;
+      left: 50%;
+      bottom: 0%;
+      transform: translate(-50%, 0%);
+      color: white;
+      width: 100%;
+      text-align: center;
+      font-weight: 600;
+      font-size: 1rem;
+      height: 40%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;
+    }
+  `;
   
   export default Popular;
