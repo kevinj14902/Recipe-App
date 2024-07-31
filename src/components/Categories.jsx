0a380c84cbab6d81e37c40ac652ec2ea9c9f6bab
biggiech33se246 <kevinj14902@gmail.com>
2024-07-31 13:27:17 -0300

    added styling to category buttons

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -6,25 +6,25 @@ import { NavLink } from "react-router-dom";
 function Categories() {
   return (
     <List>
-        <NavLink to={"/cuisines/Italian"}>
+        <StyledLink to={"/cuisines/Italian"}>
             <FaPizzaSlice/>
             <h4>Italian</h4>
-        </NavLink>
+        </StyledLink>
         
-        <NavLink to={"/cuisines/American"}>
+        <StyledLink to={"/cuisines/American"}>
             <FaHamburger/>
             <h4>American</h4>
-        </NavLink>
+        </StyledLink>
         
-        <NavLink to={"/cuisines/Chinese"}>
+        <StyledLink to={"/cuisines/Thai"}>
             <GiNoodles/>
-            <h4>Chinese</h4>
-        </NavLink>
+            <h4>Thai</h4>
+        </StyledLink>
         
-        <NavLink to={"/cuisines/Japanese"}>
+        <StyledLink to={"/cuisines/Japanese"}>
             <GiChopsticks/>
             <h4>Japanese</h4>
-        </NavLink>
+        </StyledLink>
     </List>
   )
 }
@@ -33,6 +33,41 @@ const List = styled.div`
     display: flex;
     justify-content: center;
     margin: 2rem 0rem;
+`;
+
+const StyledLink = styled(NavLink)`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    border-radius: 50%;
+    margin-right: 2rem;
+    background: linear-gradient(35deg, black, black);
+    width: 6rem;
+    height: 6rem;
+    cursor: pointer;
+
+    h4{
+        color: white;
+        font-size: 1rem;
+    }
+    
+    svg{
+        color: white;
+        font: 2rem;
+    }
+    
+    &.active{
+        background: linear-gradient(to right, #90EE90, #90EE90);
+
+        svg{
+            color: black;
+        }
+        
+        h4{
+            color: black;
+        }
+    }
 `;
 
 export default Categories
