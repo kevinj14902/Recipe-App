c1ec682560faa4baab767b544851dfcb3b25ef66
biggiech33se246 <kevinj14902@gmail.com>
2024-08-02 12:20:03 -0300

    added a search bar in components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Pages from "./pages/Pages";
 import Categories from "./components/Categories";
 import {BrowserRouter} from "react-router-dom";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Search />
         <Categories />
         <Pages />
       </BrowserRouter>
