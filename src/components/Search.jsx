3660b65e546691137b4cf4be955cddeba65d07aa
biggiech33se246 <kevinj14902@gmail.com>
2024-08-04 11:52:20 -0300

    added fully functioning search bar in Searched.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
 import styled from "styled-components";
 import { useState } from 'react';
 import {FaSearch} from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
 
 function Search() {
 
     const [input, setInput] = useState("");
+    const navigate = useNavigate();
     const submitHandler = (event) => {
         event.preventDefault();
+        navigate('/searched/'+input);
     };
 
   return (
-    <FormStyle>
+    <FormStyle onSubmit={submitHandler}>
         <div>
             <FaSearch></FaSearch>
             <input onChange={(event) => setInput(event.target.value)} type="text" value={input}/>
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Home from './Home';
 import Cuisines from './Cuisines';
+import Searched from './Searched';
+import Recipes from './Recipes';
 import {Route, Routes} from "react-router-dom";
 
 function Pages() {
@@ -10,6 +12,8 @@ function Pages() {
         <Route path="/Recipe-App" element={<Home />} />
         <Route path="/" element={<Home />} />
         <Route path="/cuisines/:type" element={<Cuisines />} />
+        <Route path="/searched/:search" element={<Searched />} />
+        <Route path="/recipes/:name" element={<Recipes />} />
       </Routes>
 
   )
